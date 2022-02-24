27757accc72f4338082bccb4d415522538b99863
liaoshiqing <liaosq@ligeit.com>
2022-02-24 09:29:12 +0800

    init project

---
+++ src/main/java/com/example/genshingachaconverter/GenshinGachaConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.genshingachaconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinGachaConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenshinGachaConverterApplication.class, args);
+    }
+
+}
